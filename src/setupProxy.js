3dd700f8e198b31512d42e6d89ea99724e6a1f2e
vaibhav-karnwal <vaibhavkarnwal2812@gmail.com>
2024-07-29 21:03:59 +0530

    url updated

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function (app) {
   app.use(
     "/v1",
     createProxyMiddleware({
-      // target: "https://backend.lyamii.com",
-      target:"http://localhost:8081",
+      target: "https://backend.lyamii.com",
+      // target:"http://localhost:8081",
       changeOrigin: true,
       secure: false,
     })
